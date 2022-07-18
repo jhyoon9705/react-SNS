ca34c310317b0f2905b0aae45226c069b639991c
jhyoon9705 <jhyoon513@naver.com>
2022-07-18 18:44:37 +0900

    PostForm & PostCard with dummy data

---
+++ prepare/front/components/PostCard.js
@@ -0,0 +1,84 @@
+import React, { useState, useCallback } from "react";
+import { Card, Popover, Button, Avatar } from "antd";
+import {
+  RetweetOutlined,
+  HeartOutlined,
+  MessageOutlined,
+  EllipsisOutlined,
+  HeartTwoTone,
+} from "@ant-design/icons";
+import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
+import PostImages from "./PostImages";
+
+const PostCard = ({ post }) => {
+  const [liked, setLiked] = useState(false);
+  const [commentFormOpened, setCommentFormOpened] = useState(false);
+  const onToggleLike = useCallback(() => {
+    setLiked((prev) => !prev); // toggle 시에 사용하는 코드
+  }, []);
+  const onToggleComment = useCallback(() => {
+    setCommentFormOpened((prev) => !prev);
+  }, []);
+  const { me } = useSelector((state) => state.user);
+  const id = me && me.id;
+  // me.id가 있으면 해당 값이 id에 들어가고, 없으면 undefined
+  // const id = me?.id; 도 가능(optional chaining / 있는지 없는지 의심이 되는 부분에서 사용)
+  return (
+    <div style={{ marginBottom: 20 }}>
+      <Card
+        cover={post.Images[0] && <PostImages images={post.Images} />}
+        actions={[
+          // 배열 안에 jsx를 넣을 때는 key값을 할당하여야 함
+          <RetweetOutlined key="retweet" />,
+          liked ? (
+            <HeartTwoTone
+              twoToneColor="#eb2f96"
+              key="heart"
+              onClick={onToggleLike}
+            />
+          ) : (
+            <HeartOutlined key="heart" onClick={onToggleLike} />
+          ),
+          <MessageOutlined key="comment" onClick={onToggleComment} />,
+          <Popover
+            key="more"
+            content={
+              <Button.Group>
+                {id && post.User.id === id ? (
+                  <>
+                    <Button>수정</Button>
+                    <Button type="danger">삭제</Button>
+                  </>
+                ) : (
+                  <Button>신고</Button>
+                )}
+              </Button.Group>
+            }
+          >
+            <EllipsisOutlined />
+          </Popover>,
+        ]}
+      >
+        <Card.Meta
+          avatar={<Avatar>{post.User.nickname[0]}</Avatar>}
+          title={post.User.nickname}
+          description={post.content}
+        />
+      </Card>
+      {commentFormOpened && <div>댓글 부분</div>}
+    </div>
+  );
+};
+
+PostCard.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    User: PropTypes.object,
+    content: PropTypes.string,
+    createdAt: PropTypes.object,
+    Comments: PropTypes.arrayOf(PropTypes.object),
+    Images: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
+};
+export default PostCard;
--- prepare/front/reducers/post.js
+++ prepare/front/reducers/post.js
@@ -1,9 +1,67 @@
 export const initialState = {
-  mainPosts: [],
+  mainPosts: [
+    {
+      id: 1,
+      User: {
+        id: 1,
+        nickname: "jhyoon",
+      },
+      content: "첫 번째 개시글 #해시태그 #익스프레스",
+      Images: [
+        {
+          src: "https://picsum.photos/id/0/200/300​",
+        },
+        {
+          src: "https://picsum.photos/id/101/200/300​",
+        },
+        {
+          src: "https://picsum.photos/id/237/200/300​",
+        },
+      ],
+      Comments: [
+        {
+          User: {
+            nickname: "nero",
+          },
+          content: "hello~~~!",
+        },
+        {
+          User: {
+            nickname: "hero",
+          },
+          content: "Nice to meet you!",
+        },
+      ],
+    },
+  ],
+  imagePaths: [],
+  postAdded: false,
+};
+
+const ADD_POST = "ADD_POST";
+export const addPost = {
+  type: ADD_POST,
+};
+
+const dummyPost = {
+  id: 2,
+  content: "This is a dummy data",
+  User: {
+    id: 1,
+    nickname: "jhyoon",
+  },
+  Images: [],
+  Comments: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_POST:
+      return {
+        ...state,
+        mainPosts: [dummyPost, ...state.mainPosts],
+        postAdded: true,
+      };
     default:
       return state;
   }
