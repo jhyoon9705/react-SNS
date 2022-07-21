03d9a4d82bc958965ecef3491822addbe2b3813a
jhyoon9705 <jhyoon513@naver.com>
2022-07-21 23:31:19 +0900

    Redux-saga effects(2)

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -28,7 +28,17 @@
 // g.next(); -> return value: 3
 // ...
 
-import { all, fork, call, put, take } from "redux-saga/effects";
+import {
+  all,
+  fork,
+  call,
+  put,
+  take,
+  takeEvery,
+  takeLatest,
+  throttle,
+  delay,
+} from "redux-saga/effects";
 import axios from "axios";
 
 // effect들 앞에 yield 키워드를 붙여주는 이유
@@ -40,8 +50,13 @@ function logInAPI(data) {
 
 function* logIn(action) {
   try {
-    const result = yield call(logInAPI, action.data); // fork를 쓰면 비동기, call은 동기 함수 호출
-    // fork와 call의 두 번째 인자부터는 매개변수 = loginAPI(action.data)
+    // // 나중에 서버 만들면 사용
+    // const result = yield call(logInAPI, action.data); // fork를 쓰면 비동기, call은 동기 함수 호출
+    // // fork와 call의 두 번째 인자부터는 매개변수 = loginAPI(action.data)
+
+    // 서버 만들기 전 (for dev), delay effect로 비동기적인 효과 사용
+    yield delay(1000);
+
     yield put({
       type: "LOG_IN_SUCCESS",
       data: result.data,
@@ -60,7 +75,11 @@ function logOutAPI() {
 
 function* logOut() {
   try {
-    const result = yield call(logOutAPI); // fork를 쓰면 비동기, call은 동기 함수 호출
+    //const result = yield call(logOutAPI); // fork를 쓰면 비동기, call은 동기 함수 호출
+
+    // 서버 만들기 전 (for dev), delay effect로 비동기적인 효과 사용
+    yield delay(1000);
+
     yield put({
       type: "LOG_OUT_SUCCESS",
       data: result.data,
@@ -79,7 +98,11 @@ function addPostAPI(data) {
 
 function* addPost() {
   try {
-    const result = yield call(addPostAPI, action.data); // fork를 쓰면 비동기, call은 동기 함수 호출
+    // const result = yield call(addPostAPI, action.data); // fork를 쓰면 비동기, call은 동기 함수 호출
+
+    // 서버 만들기 전 (for dev), delay effect로 비동기적인 효과 사용
+    yield delay(1000);
+
     yield put({
       type: "ADD_POST_SUCCESS",
       data: result.data,
@@ -93,15 +116,35 @@ function* addPost() {
 }
 
 function* watchLogIn() {
-  yield take("LOG_IN_REQUEST", logIn);
+  // while (true) { // while(true)가 없으면 1회용이 됨(한번 로그인 하면 watchLogIn이 없어짐)(동기)
+  //   yield take("LOG_IN_REQUEST", logIn);
+  // }
+
+  // while(true) 대신에 takeEvery 사용 가능(비동기)
+  // yield takeEvery("LOG_IN_REQUEST", logIn);
+
+  // takeLatest는 여러번 눌렸을 시에 왼료되지 않은 작업들 중 마지막 클릭(요청)만 실행함(<->takeEvery)
+  // 정확히는, 로딩중인 같은 요청을 취소하는 것이 아니라 서버로 부터 온 응답을 취소하는 것
+  // 따라서 서버쪽에서 서버에 같은 데이터가 중복 저장되지 않았는지 확인이 필요함
+  // 첫번째 클릭만 실행하려면 takeLeading 사용
+  yield takeLatest("LOG_IN_REQUEST", logIn);
+
+  // // 2초동안 LOG_IN_REQUEST는 딱 1번만 실행하도록 제한, DOS 공격 방지 가능
+  // yield throttle("LOG_IN_REQUEST", logIn, 2000);
 }
 
 function* watchLogOut() {
-  yield take("LOG_OUT_REQUEST", logOut);
+  // while (true) {
+  //   yield take("LOG_OUT_REQUEST", logOut);
+  // }
+  yield takeLatest("LOG_OUT_REQUEST", logOut);
 }
 
 function* watchAddPost() {
-  yield take("ADD_POST_REQUEST", addPost);
+  // while (true) {
+  //   yield take("ADD_POST_REQUEST", addPost);
+  // }
+  yield takeLatest("ADD_POST_REQUEST", addPost);
 }
 
 // generator
@@ -115,3 +158,5 @@ export default function* rootSaga() {
     fork(watchAddPost),
   ]);
 }
+
+// cf) 쓰로틀링 vs 디바운싱 (블로그 참고하여 추후 대체)
