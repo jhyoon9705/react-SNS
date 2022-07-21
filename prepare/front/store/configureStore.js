e6d8b6760d86b6d554e84c5de57684b342cd7007
jhyoon9705 <jhyoon513@naver.com>
2022-07-21 18:01:45 +0900

    Install redux-saga, generator

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -2,16 +2,19 @@
 import { createWrapper } from "next-redux-wrapper";
 import { createStore, applyMiddleware, compose } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension"; // redux history 기록 용도(개발 모드일떄만 사용)
-import thunkMiddleware from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 import reducer from "../reducers";
+import rootSaga from "../sagas";
 
 const configureStore = () => {
-  const middlewares = [thunkMiddleware];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
       : composeWithDevTools(applyMiddleware(...middlewares));
   const store = createStore(reducer, enhancer);
+  store.sagaTask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
