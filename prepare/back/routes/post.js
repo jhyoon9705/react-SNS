dfe648848be21712ba9d5a0aa1022081303be4cf
jhyoon9705 <jhyoon513@naver.com>
2022-08-18 17:27:48 +0900

    Hashtags

--- prepare/back/routes/post.js
+++ prepare/back/routes/post.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 
-const { Post, Comment, User, Image } = require('../models');
+const { Post, Comment, User, Image, Hashtag } = require('../models');
 const { isLoggedIn } = require('./middlewares');
 
 const router = express.Router();
@@ -39,10 +39,17 @@ router.post('/images', isLoggedIn, upload.array('image'), (req, res, next) => {
 
 router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
   try {
+    const hastags = req.body.content.match(/#[^\s#]+/g);
     const post = await Post.create({
       content: req.body.content,
       UserId: req.user.id,
     });
+    if (hastags) {
+      const result = await Promise.all(hastags.map((tag) => Hashtag.findOrCreate({ 
+        where: { name: tag.slice(1).toLowerCase() 
+      }}))); // findOrCreate의 result: [#노드, true], [#리액트, true] (두 번째값은 find인지 create인지) 
+      await post.addHashtags(result.map((v) => v[0]));
+    }
     if (req.body.image) { // 이미지를 올린 경우
       if (Array.isArray(req.body.image)) { // 이미지를 여러 개 올리면 [1.png, 2.png]
         const images = await Promise.all(req.body.image.map((image) => Image.create({src: image})));
